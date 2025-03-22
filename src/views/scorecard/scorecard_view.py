0a51159e7571b514c5d0560d307aeff7aa426087
jducifuentes <jducifuentes@gmail.com>
2025-03-22 23:48:49 +0100

    ver 0.2

--- src/views/scorecard/scorecard_view.py
+++ src/views/scorecard/scorecard_view.py
@@ -7,6 +7,8 @@ from src.controllers.player_controller import PlayerController
 from src.controllers.course_controller import CourseController
 from src.views.base_view import BaseView
 from src.views.utils import format_title, format_info, clear_screen, pause
+from src.utils.formatters import format_menu_option, format_error
+from src.utils.helpers_simple import get_number_input
 from src.views.scorecard.scorecard_list_view import ScorecardListView
 from src.views.scorecard.scorecard_edit_view import ScorecardEditView
 from src.views.scorecard.scorecard_create_view import ScorecardCreateView
@@ -61,36 +63,31 @@ class ScorecardView(BaseView):
             clear_screen()
             print(format_title("GESTIÓN DE TARJETAS"))
             
-            print("\nOpciones disponibles:")
-            print("  1. Ver todas las tarjetas")
-            print("  2. Buscar tarjetas")
-            print("  3. Crear nueva tarjeta")
-            print("  4. Exportar tarjetas")
-            print("  0. Volver al menú principal")
+            print(f"\n{Fore.YELLOW}Opciones:{Style.RESET_ALL}")
+            print(format_menu_option("1", "Ver todas las tarjetas"))
+            print(format_menu_option("2", "Buscar tarjetas"))
+            print(format_menu_option("3", "Crear nueva tarjeta"))
+            print(format_menu_option("4", "Exportar tarjetas"))
+            print(format_menu_option("0", "Volver al menú principal"))
             
-            option = input("\nSeleccione una opción: ")
+            option = get_number_input("Seleccione una opción", default=0, min_value=0, max_value=4, allow_float=False)
             
-            if option == "1":
+            if option == 0:
+                break
+            elif option == 1:
                 selected_scorecard = self.show_all_scorecards()
                 if selected_scorecard:
                     self.edit_view.show_scorecard_options(selected_scorecard)
-            
-            elif option == "2":
+            elif option == 2:
                 selected_scorecard = self.search_scorecards()
                 if selected_scorecard:
                     self.edit_view.show_scorecard_options(selected_scorecard)
-            
-            elif option == "3":
+            elif option == 3:
                 self.create_scorecard()
-            
-            elif option == "4":
+            elif option == 4:
                 self.export_view.show_export_menu()
-            
-            elif option == "0":
-                break
-            
             else:
-                print(f"{Fore.RED}Opción inválida.{Style.RESET_ALL}")
+                print(format_error("Opción inválida."))
                 pause()
     
     def show_all_scorecards(self):
